5bbe21997fc5f5e73eec3c3a74bbaefa2fa1e3df
goflutterjava <goflutterjava@gmail.com>
2022-12-04 16:23:28 +0800

    add node exporter start script

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -2,3 +2,4 @@
 
 DIR="$( cd "$( dirname "$0"  )" && pwd  )"
 mkdir -p $NODE_EXPORTER_HOME/logs
+nohup $NODE_EXPORTER_HOME/node_exporter >>$NODE_EXPORTER_HOME/logs/prometheus.stdout.log 2>>$NODE_EXPORTER_HOME/logs/prometheus.stderr.log &
